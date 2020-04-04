a6bd5770a9eb6da5c3dc474ea42da7da918ccf73
Micah Rassi <micahrassi@gmail.com>
2020-04-04 00:56:07 -0400

    Make default auton just a drive movement again

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 import mctp as robot
 
 robot.init(robot.Launcher.PUNCHER)
-
-robot.drive(-2000)
-#robot.shoot()
-#robot.turn(-45)
-#robot.angle(-70)
-#robot.drive(1350)
-#robot.turn(90)
-#robot.cap_flip()
-#robot.drive(1000)
-#robot.turn(-40)
-#robot.drive(-1000)
-#robot.angle(60)
-#robot.turn(20)
-#robot.shoot()
-
+robot.drive(1000)
 robot.end()
